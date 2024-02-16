b11dd7303215a092f4a5223e6bbcd33f9e3b3c11
PhilippXhonneux <philipp.xhonneux@gmx.de>
2024-02-16 12:00:58 +0100

    Added Java Docs and made Separotor for CSV-format variable

--- src/main/java/Data/Datastructur.java
+++ src/main/java/Data/Datastructur.java
@@ -1,8 +1,13 @@
 package Data;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 
+/**
+ * Basis Class for all Datastructur-objects
+ *
+ * @author Philipp Xhonneux
+ * @version 1.5.0
+ */
 public abstract class Datastructur {
 
 	private int intValue;
@@ -10,7 +15,13 @@ public abstract class Datastructur {
 	private double doubleValue;
 
 
-	public String ToCSVString() {
+	/**
+	 * Gives back a String in CSV-format.
+	 *
+	 * @param separator the saparator used for the CSV-Format
+	 * @return the string
+	 */
+	public String ToCSVString(String separator) {
 		StringBuilder result = new StringBuilder();
 		Class<?> clazz = getClass();
 
@@ -18,7 +29,7 @@ public abstract class Datastructur {
 			for (Field field : clazz.getDeclaredFields()) {
 				field.setAccessible(true); // Zugriff auf private Felder erlauben
 				result.append(field.get(this))
-						.append(";");
+						.append(separator);
 			}
 			// Entferne das letzte Komma
 			if (result.length() > 0) {
