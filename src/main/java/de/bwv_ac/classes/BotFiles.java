b5499c5ef94bdb52c319a012da0f8408935849c1
ratajczak_ale <alexander.ratajczak2005@gmail.com>
2024-02-16 13:14:23 +0100

    added BotFiles, to write and read csv files

--- src/main/java/de/bwv_ac/classes/BotFiles.java
+++ src/main/java/de/bwv_ac/classes/BotFiles.java
@@ -1,12 +1,20 @@
 package de.bwv_ac.classes;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import Data.Datastructur;
+import jdk.jshell.spi.ExecutionControl;
+
+import javax.xml.crypto.Data;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BotFiles
 {
+    private static String delimiter = ";";
+
+    public static void setDelimiter(String delimiter){
+        BotFiles.delimiter = delimiter;
+    }
     public static void CSVWriter(String filePath, ArrayList<ArrayList<String>> csv, boolean hasHeader)
     {
         File file = new File(filePath);
@@ -20,11 +28,10 @@ public class BotFiles
                 {
                     continue;
                 }
-                String tempLine = ConvertToCSVLine(row);
+                String tempLine = convertToCSVLine(row);
                 writer.append(tempLine);
                 writer.flush();
             }
-
 
             writer.close(); // Schließt den Writer
         }
@@ -32,13 +39,41 @@ public class BotFiles
         {
             throw new RuntimeException(e);
         }
+        catch (ExecutionControl.NotImplementedException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
-    private static String ConvertToCSVLine(ArrayList<String> row)
+    public static <T extends Datastructur> ArrayList<T> CSVReader(String filePath, boolean hasHeader, Class<T> clazz) throws FileNotFoundException
     {
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter(delimiter);
+
+        ArrayList<T> output = new ArrayList<>();
+        try
+        {
+            while(sc.hasNext())
+            {
+                String line = sc.nextLine();
+
+                T temp = clazz.newInstance();
+                temp.FromCSVStringToObject(line, delimiter);
+            }
+
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    private static String convertToCSVLine(ArrayList<String> row) throws ExecutionControl.NotImplementedException {
         for (String columns : row)
         {
 
         }
-        return null;
+        throw new ExecutionControl.NotImplementedException("TODO");
     }
 }
