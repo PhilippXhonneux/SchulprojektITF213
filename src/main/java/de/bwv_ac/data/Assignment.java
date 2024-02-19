142e57fca678f86a25f540f5629579187be377df
pgoebel <philipp.goebel@amadeus.com>
2024-02-19 11:23:09 +0100

    Datenstructuren der einzelnen Exceltabellen. BOT 1-4

---
+++ src/main/java/de/bwv_ac/data/Assignment.java
@@ -0,0 +1,4 @@
+package de.bwv_ac.data;
+
+public class Assignment extends Datastructur{
+}
---
+++ src/main/java/de/bwv_ac/data/Timetable.java
@@ -0,0 +1,61 @@
+package de.bwv_ac.data;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+
+/**
+ * Datastructur for Stundenplan BOT2
+ *
+ * @author Philipp Goebel
+ * @version 1.0.0
+ */
+
+public class Timetable extends Datastructur {
+    private Integer ID;
+    private String company;
+    private String room;
+    private LocalTime[] time;
+
+
+    public static final LocalTime[] TIME_A = {LocalTime.of(8, 45), LocalTime.of(9, 30)};
+    public static final LocalTime[] TIME_B = {LocalTime.of(9, 50), LocalTime.of(10, 30)};
+    public static final LocalTime[] TIME_C = {LocalTime.of(10, 35), LocalTime.of(11, 20)};
+    public static final LocalTime[] TIME_D = {LocalTime.of(11, 40), LocalTime.of(12, 25)};
+    public static final LocalTime[] TIME_E = {LocalTime.of(12, 25), LocalTime.of(13, 10)};
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public LocalTime[] getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime[] time) {
+        if (!Arrays.equals(time, TIME_A) && !Arrays.equals(time, TIME_B) && !Arrays.equals(time, TIME_C)
+                && !Arrays.equals(time, TIME_D) && !Arrays.equals(time, TIME_E)) {
+            throw new IllegalArgumentException("Invalid time slot. Time must be one of the predefined time slots A to E.");
+        }
+        this.time = time;
+    }
+}
