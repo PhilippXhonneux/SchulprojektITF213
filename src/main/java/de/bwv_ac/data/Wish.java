46af8ff0fd6f2810b2b827eb115117d6eb4cb844
PhilippXhonneux <philipp.xhonneux@gmx.de>
2024-02-23 10:23:47 +0100

    Changed Name

--- src/main/java/de/bwv_ac/data/Wish.java
+++ src/main/java/de/bwv_ac/data/Wish.java
@@ -6,7 +6,7 @@ package de.bwv_ac.data;
  * @author Philipp Goebel
  * @version 1.0.0
  */
-public class Wishes extends Datastructure {
+public class Wish extends Datastructure {
     private String className;
     private String surname;
     private String name;
