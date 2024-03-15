8084b302e36ba02b9d3ce14adfc4513310a47aa7
Robin G <50486040+robio10000@users.noreply.github.com>
2024-03-15 17:24:46 +0100

    CHG: Wishlist: import

--- src/main/java/de/bwv_ac/classes/Bot2Controller.java
+++ src/main/java/de/bwv_ac/classes/Bot2Controller.java
@@ -1,9 +1,17 @@
 package de.bwv_ac.classes;
 
+import de.bwv_ac.data.Wish;
 import de.bwv_ac.data.Wishes;
 import de.bwv_ac.view.bot_2.WishList;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Bot2Controller {
 
@@ -20,15 +28,75 @@ public class Bot2Controller {
         this.wishes = wishes;
         wishList = new WishList();
         wishes.addObserver(wishList);
+        wishList.setActionListener("import", onImportAction);
 
         wishes.notifyObservers();
     }
 
     /**
-     * Get the panel for bot 1
+     * Get the panel for bot 2
      * @return a JPanel
      */
     public Component getPanel() {
         return wishList;
     }
+
+    /**
+     * Handle User Action<br>
+     * Configure and show a JFileChooser.<br>
+     * After selecting a file to import,<br>
+     * read the first line and the body separately<br>
+     * and import them to the wishes data model
+     */
+    ActionListener onImportAction = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setDialogTitle("Wähle eine Kommaseparierte Liste im .CSV format aus");
+            chooser.setApproveButtonText("Auswählen");
+            chooser.setMultiSelectionEnabled(false);
+            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            FileFilter filter = new FileFilter() {
+                @Override
+                public boolean accept(File f) {
+                    String name = f.getName();
+
+                    if(f.isDirectory())
+                        return true;
+
+                    return name.endsWith(".csv");
+                }
+
+                @Override
+                public String getDescription() {
+                    return "CSV Datei / *.csv";
+                }
+            };
+            chooser.setFileFilter(filter);
+
+            int rval = chooser.showOpenDialog(wishList);
+
+            if(rval != JFileChooser.APPROVE_OPTION){
+                return;
+            }
+
+            File f = chooser.getSelectedFile();
+
+            try {
+                ArrayList<Wish> wish = CSVReader.read(f.getAbsolutePath(),true, Wish.class);
+                String[] columns = CSVReader.getFirstLine(f.getAbsolutePath());
+                wishes.add(wish, columns);
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+                // TODO: Error handling
+            }
+        }
+    };
+
+    ActionListener onExportListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
 }
